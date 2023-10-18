7c354a009e62aefbcb8bb9b544455d5d9f0a9587
Thabata Pontes <tathabatapontes@hotmail.com>
2023-10-18 20:35:51 -0300

    Add next steps

--- main.py
+++ main.py
@@ -7,16 +7,39 @@ import csv
 import requests
 from pprint import pprint
 
+from requests import Response
 
-def transform_to_csv(file: object) -> None:
-    header = ['ID Nation', 'Nation', 'ID Year', 'Year', 'Population', 'Slug Nation']
-    data = [
-        ['01000US', 'United States', '2020', '2020', '326569308', 'united-states']
-    ]
+# Functional way: use a Map (format -> function)
+available_formats = ['csv', 'json', 'parquet']
 
-    writer = csv.writer(file)
-    writer.writerow(header)
-    writer.writerow(data)
+
+# OOO way: create a class for each format
+
+
+def transform_to_csv(response: Response, file_format):
+    response_data = response.json()
+    header = response_data['data'][0].keys()
+    data = []
+
+    for row in response_data['data']:
+        values = [row[key] for key in header]
+        data.append(values)
+
+    with open(f'{file_format}_file', 'w', encoding='UTF8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(header)
+        for row in data:
+            writer.writerow(row)
+
+
+# TODO create unit tests
+def transform_to_required_format(response: Response, file_format: str) -> None:
+    if file_format == 'csv':
+        transform_to_csv(response, file_format)
+    elif file_format == 'json':
+        with open(f'{file_format}_file', 'w', encoding='UTF8') as file:
+            writer = file.write(response.text)
 
 
 def print_hi(name):
@@ -27,11 +50,8 @@ def print_hi(name):
 
         if response.status_code == 200:
             # The request was successful
-            file = open("usa_population.json", "w")
-            # file.write(response.text)
-            transform_to_csv(file)
-            file.close()
-            # pprint(response.text)
+            file_format = 'csv'
+            transform_to_required_format(response, file_format)
         else:
             # Handle errors
             print("Request failed with status code:", response.status_code)
